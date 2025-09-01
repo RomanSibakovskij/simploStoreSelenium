7c486a95cf74f64076001637a05efe23d628a3a7
RomanSibakovskij <benutzer373@gmail.com>
2025-09-01 15:11:22 +0300

    Add empty AccountInfoPageNoSingularInput utility sheet

---
+++ src/main/java/cz/simploshop/demo/invalidscenarios/accountinfopage/AccountInfoPageNoSingularInput.java
@@ -0,0 +1,12 @@
+package cz.simploshop.demo.invalidscenarios.accountinfopage;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class AccountInfoPageNoSingularInput extends BasePage {
+
+
+    public AccountInfoPageNoSingularInput(WebDriver driver) {super(driver);}
+
+
+}
