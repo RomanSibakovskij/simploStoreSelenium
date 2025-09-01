b52cfacf8b8c3f849bd42c14468869ee7271cfaf
RomanSibakovskij <benutzer373@gmail.com>
2025-09-01 15:37:48 +0300

    Add empty AccountInfoPageTooShortSingularInput utility sheet

---
+++ src/main/java/cz/simploshop/demo/invalidscenarios/accountinfopage/AccountInfoPageTooShortSingularInput.java
@@ -0,0 +1,12 @@
+package cz.simploshop.demo.invalidscenarios.accountinfopage;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class AccountInfoPageTooShortSingularInput extends BasePage{
+
+
+    public AccountInfoPageTooShortSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
