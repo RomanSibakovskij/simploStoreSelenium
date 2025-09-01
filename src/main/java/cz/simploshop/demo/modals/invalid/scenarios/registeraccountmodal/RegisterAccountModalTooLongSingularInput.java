fe3892661d8bfb0a0d862263e9e3904fc9c1bbb3
RomanSibakovskij <benutzer373@gmail.com>
2025-09-01 07:13:47 +0300

    Add empty RegisterAccountModalTooLongSingularInput utility sheet

---
+++ src/main/java/cz/simploshop/demo/modals/invalid/scenarios/registeraccountmodal/RegisterAccountModalTooLongSingularInput.java
@@ -0,0 +1,12 @@
+package cz.simploshop.demo.modals.invalid.scenarios.registeraccountmodal;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class RegisterAccountModalTooLongSingularInput extends BasePage{
+
+
+    public RegisterAccountModalTooLongSingularInput(WebDriver driver) {super(driver);}
+
+
+}
