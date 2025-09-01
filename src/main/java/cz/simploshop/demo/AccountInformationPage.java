891b04abd625dc9ad47cc006b1ad860701b7ba3f
RomanSibakovskij <benutzer373@gmail.com>
2025-09-01 14:51:03 +0300

    Add empty AccountInformationPage sheet

---
+++ src/main/java/cz/simploshop/demo/AccountInformationPage.java
@@ -0,0 +1,13 @@
+package cz.simploshop.demo;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class AccountInformationPage extends BasePage {
+
+
+
+    public AccountInformationPage(WebDriver driver) {super(driver);}
+
+    
+}
