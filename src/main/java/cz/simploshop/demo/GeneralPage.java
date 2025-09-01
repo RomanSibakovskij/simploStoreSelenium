64a4cbf19527b3f7e607f845dcac0153b1a23afa
RomanSibakovskij <benutzer373@gmail.com>
2025-09-01 05:15:41 +0300

    Add empty GeneralPage sheet

---
+++ src/main/java/cz/simploshop/demo/GeneralPage.java
@@ -0,0 +1,12 @@
+package cz.simploshop.demo;
+
+import cz.simploshop.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+
+public class GeneralPage extends BasePage {
+
+
+    public GeneralPage(WebDriver driver) {super(driver);}
+
+    
+}
