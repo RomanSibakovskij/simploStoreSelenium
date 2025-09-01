b94cad3a55943b136f0a41f8ddc302b29b702836
RomanSibakovskij <benutzer373@gmail.com>
2025-09-01 05:19:09 +0300

    Add empty HomePage sheet

---
+++ src/main/java/cz/simploshop/demo/HomePage.java
@@ -0,0 +1,12 @@
+package cz.simploshop.demo;
+
+import cz.simploshop.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+
+public class HomePage extends BasePage {
+
+
+    public HomePage(WebDriver driver) {super(driver);}
+
+    
+}
