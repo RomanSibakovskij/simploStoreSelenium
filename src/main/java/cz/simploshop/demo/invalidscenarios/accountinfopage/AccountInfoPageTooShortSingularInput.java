2cf33e42fd262e4dc45b137f7a7002260bf4fe71
RomanSibakovskij <benutzer373@gmail.com>
2025-09-01 15:39:13 +0300

    Add relevant web elements for AccountInfoPageTooShortSingularInput utility sheet

--- src/main/java/cz/simploshop/demo/invalidscenarios/accountinfopage/AccountInfoPageTooShortSingularInput.java
+++ src/main/java/cz/simploshop/demo/invalidscenarios/accountinfopage/AccountInfoPageTooShortSingularInput.java
@@ -3,10 +3,26 @@ package cz.simploshop.demo.invalidscenarios.accountinfopage;
 import cz.simploshop.demo.utilities.*;
 import org.openqa.selenium.*;
 
+import org.openqa.selenium.support.FindBy;
+
 public class AccountInfoPageTooShortSingularInput extends BasePage{
 
+    //relevant web elements
+    @FindBy(xpath = "//main//section[2]//input[@id='v-0-4-0-0-1']")
+    private WebElement accountInfoPageFirstNameInputField;
+    @FindBy(xpath = "//main//section[2]//input[@id='v-0-4-0-0-7']")
+    private WebElement accountInfoPageLastNameInputField;
+    @FindBy(xpath = "//main//section[2]//input[@id='v-0-4-0-0-13']")
+    private WebElement accountInfoPageEmailInputField;
+    //password section
+    @FindBy(xpath = "//main//section[3]//input[@id='v-0-4-0-0-25']")
+    private WebElement accountInfoPagePasswordInputField;
+    @FindBy(xpath = "//main//section[3]//input[@id='v-0-4-0-0-31']")
+    private WebElement accountInfoPageNewPasswordInputField;
+    @FindBy(xpath = "//main//section[3]//input[@id='v-0-4-0-0-37']")
+    private WebElement accountInfoPageConfirmNewPasswordInputField;
 
     public AccountInfoPageTooShortSingularInput(WebDriver driver) {super(driver);}
 
-    
+
 }
