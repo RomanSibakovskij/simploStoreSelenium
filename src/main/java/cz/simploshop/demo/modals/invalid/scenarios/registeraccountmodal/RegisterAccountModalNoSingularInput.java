5ffaf99fda544340395f4e881bcddba70d38cd00
RomanSibakovskij <benutzer373@gmail.com>
2025-09-01 06:11:18 +0300

    Add empty RegisterAccountModalNoSingularInput utility sheet

---
+++ src/main/java/cz/simploshop/demo/modals/invalid/scenarios/registeraccountmodal/RegisterAccountModalNoSingularInput.java
@@ -0,0 +1,12 @@
+package cz.simploshop.demo.modals.invalid.scenarios.registeraccountmodal;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class RegisterAccountModalNoSingularInput extends BasePage{
+
+
+    public RegisterAccountModalNoSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
