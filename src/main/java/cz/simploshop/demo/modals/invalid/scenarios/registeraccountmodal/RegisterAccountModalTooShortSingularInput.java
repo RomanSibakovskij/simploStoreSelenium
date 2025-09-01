9f22f3967461e75c8e6e38fdbb18b55418e098b5
RomanSibakovskij <benutzer373@gmail.com>
2025-09-01 06:44:35 +0300

    Add relevant web elements for RegisterAccountModalTooShortSingularInput utility sheet

--- src/main/java/cz/simploshop/demo/modals/invalid/scenarios/registeraccountmodal/RegisterAccountModalTooShortSingularInput.java
+++ src/main/java/cz/simploshop/demo/modals/invalid/scenarios/registeraccountmodal/RegisterAccountModalTooShortSingularInput.java
@@ -2,9 +2,21 @@ package cz.simploshop.demo.modals.invalid.scenarios.registeraccountmodal;
 
 import cz.simploshop.demo.utilities.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
 
 public class RegisterAccountModalTooShortSingularInput extends BasePage{
 
+    //relevant web elements
+    @FindBy(xpath = "//div[@class='sim-auth-form__wrapper']//form//input[@id='v-0-3-2']")
+    private WebElement registerAccountModalFirstNameInputField;
+    @FindBy(xpath = "//div[@class='sim-auth-form__wrapper']//form//input[@id='v-0-3-8']")
+    private WebElement registerAccountModalLastNameInputField;
+    @FindBy(xpath = "//div[@class='sim-auth-form__wrapper']//form//input[@id='v-0-3-14']")
+    private WebElement registerAccountModalEmailInputField;
+    @FindBy(xpath = "//div[@class='sim-auth-form__wrapper']//form//input[@id='v-0-3-20']")
+    private WebElement registerAccountModalPasswordInputField;
+    @FindBy(xpath = "//div[@class='sim-auth-form__wrapper']//form//input[@id='v-0-3-26']")
+    private WebElement registerAccountModalConfirmPasswordInputField;
 
     public RegisterAccountModalTooShortSingularInput(WebDriver driver) {super(driver);}
 
