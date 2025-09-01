dfce5dd04db01661d6d37b0bf087d145a5718fb9
RomanSibakovskij <benutzer373@gmail.com>
2025-09-01 05:24:48 +0300

    Add empty TestMethods utility sheet

---
+++ src/test/java/cz/simploshop/demo/utilities/TestMethods.java
@@ -0,0 +1,11 @@
+package cz.simploshop.demo.utilities;
+
+import cz.simploshop.demo.*;
+import cz.simploshop.demo.screenshot.*;
+
+
+public class TestMethods extends BaseTest implements ScreenshotMethod {
+
+
+
+}
