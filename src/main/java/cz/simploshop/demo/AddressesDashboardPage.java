cf9594c8d5e3a099dd000934369b2b185641a0ff
RomanSibakovskij <benutzer373@gmail.com>
2025-09-02 04:32:37 +0300

    Add empty AddressesDashboardPage sheet

---
+++ src/main/java/cz/simploshop/demo/AddressesDashboardPage.java
@@ -0,0 +1,13 @@
+package cz.simploshop.demo;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class AddressesDashboardPage extends BasePage {
+
+
+
+    public AddressesDashboardPage(WebDriver driver) {super(driver);}
+
+
+}
