de4668120f83282f848fa0f6c2846db3f19fff98
RomanSibakovskij <benutzer373@gmail.com>
2025-09-02 05:44:57 +0300

    Add empty AddNewAddressModalTooShortSingularInput utility sheet

---
+++ src/main/java/cz/simploshop/demo/modals/invalid/scenarios/addnewaddressmodal/AddNewAddressModalTooShortSingularInput.java
@@ -0,0 +1,14 @@
+package cz.simploshop.demo.modals.invalid.scenarios.addnewaddressmodal;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class AddNewAddressModalTooShortSingularInput extends BasePage{
+
+
+
+
+    public AddNewAddressModalTooShortSingularInput(WebDriver driver) {super(driver);}
+
+
+}
