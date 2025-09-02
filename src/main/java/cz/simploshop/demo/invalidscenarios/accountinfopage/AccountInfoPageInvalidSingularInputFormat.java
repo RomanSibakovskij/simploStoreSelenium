f6659ec83b3c3bb967623386e6e31058cc06ed1a
RomanSibakovskij <benutzer373@gmail.com>
2025-09-02 03:47:39 +0300

    Add empty AccountInfoPageInvalidSingularInputFormat utility sheet

---
+++ src/main/java/cz/simploshop/demo/invalidscenarios/accountinfopage/AccountInfoPageInvalidSingularInputFormat.java
@@ -0,0 +1,14 @@
+package cz.simploshop.demo.invalidscenarios.accountinfopage;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class AccountInfoPageInvalidSingularInputFormat extends BasePage{
+
+
+
+    public AccountInfoPageInvalidSingularInputFormat(WebDriver driver) {super(driver);}
+
+
+
+}
