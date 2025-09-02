27f991e7a9c64b1c6f04fb112effd677a3ddd683
RomanSibakovskij <benutzer373@gmail.com>
2025-09-02 05:45:56 +0300

    Add relevant web elements for AddNewAddressModalTooShortSingularInput utility sheet

--- src/main/java/cz/simploshop/demo/modals/invalid/scenarios/addnewaddressmodal/AddNewAddressModalTooShortSingularInput.java
+++ src/main/java/cz/simploshop/demo/modals/invalid/scenarios/addnewaddressmodal/AddNewAddressModalTooShortSingularInput.java
@@ -2,11 +2,23 @@ package cz.simploshop.demo.modals.invalid.scenarios.addnewaddressmodal;
 
 import cz.simploshop.demo.utilities.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
 
 public class AddNewAddressModalTooShortSingularInput extends BasePage{
 
-
-
+    //relevant web elements
+    @FindBy(xpath = "//div[@class='sim-modal__content']//div[@class='sim-form-row'][1]/div[1]//input")
+    private WebElement addNewAddressModalFirstNameInputField;
+    @FindBy(xpath = "//div[@class='sim-modal__content']//div[@class='sim-form-row'][1]/div[2]//input")
+    private WebElement addNewAddressModalLastNameInputField;
+    @FindBy(xpath = "//div[@class='sim-modal__content']//div[@class='sim-form-row'][2]//input")
+    private WebElement addNewAddressModalStreetInputField;
+    @FindBy(xpath = "//div[@class='sim-modal__content']//div[@class='sim-form-row'][3]/div[1]//input")
+    private WebElement addNewAddressModalZipCodeInputField;
+    @FindBy(xpath = "//div[@class='sim-modal__content']//div[@class='sim-form-row'][3]/div[2]//input")
+    private WebElement addNewAddressModalCityInputField;
+    @FindBy(xpath = "//form/div[4]//input")
+    private WebElement addNewAddressModalPhoneInputField;
 
     public AddNewAddressModalTooShortSingularInput(WebDriver driver) {super(driver);}
 
