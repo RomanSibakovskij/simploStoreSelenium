1abd230668fe45f9e7fdd72f60b922c0db60f8d1
RomanSibakovskij <benutzer373@gmail.com>
2025-09-02 04:38:17 +0300

    Add empty AddNewAddressModal utility sheet

---
+++ src/main/java/cz/simploshop/demo/modals/AddNewAddressModal.java
@@ -0,0 +1,12 @@
+package cz.simploshop.demo.modals;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class AddNewAddressModal extends BasePage{
+
+
+    public AddNewAddressModal(WebDriver driver) {super(driver);}
+
+
+}
