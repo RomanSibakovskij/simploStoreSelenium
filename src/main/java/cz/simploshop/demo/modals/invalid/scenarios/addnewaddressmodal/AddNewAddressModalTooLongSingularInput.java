a3258677c446b4bb554e393a4de21f39c05870d9
RomanSibakovskij <benutzer373@gmail.com>
2025-09-02 18:52:43 +0300

    Add empty AddNewAddressModalTooLongSingularInput utility sheet

---
+++ src/main/java/cz/simploshop/demo/modals/invalid/scenarios/addnewaddressmodal/AddNewAddressModalTooLongSingularInput.java
@@ -0,0 +1,15 @@
+package cz.simploshop.demo.modals.invalid.scenarios.addnewaddressmodal;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class AddNewAddressModalTooLongSingularInput extends BasePage{
+
+
+
+
+    public AddNewAddressModalTooLongSingularInput(WebDriver driver) {super(driver);}
+
+
+    
+}
