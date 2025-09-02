c2d822ce22bb996d57cf1559343b7965ac5a34a4
RomanSibakovskij <benutzer373@gmail.com>
2025-09-02 03:48:59 +0300

    Add relevant web elements for AccountInfoPageInvalidSingularInputFormat utility sheet

--- src/main/java/cz/simploshop/demo/invalidscenarios/accountinfopage/AccountInfoPageInvalidSingularInputFormat.java
+++ src/main/java/cz/simploshop/demo/invalidscenarios/accountinfopage/AccountInfoPageInvalidSingularInputFormat.java
@@ -2,10 +2,24 @@ package cz.simploshop.demo.invalidscenarios.accountinfopage;
 
 import cz.simploshop.demo.utilities.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
 
 public class AccountInfoPageInvalidSingularInputFormat extends BasePage{
 
-
+    //relevant web elements
+    @FindBy(xpath = "//main//section[2]//input[@id='v-0-4-0-0-1']")
+    private WebElement accountInfoPageFirstNameInputField;
+    @FindBy(xpath = "//main//section[2]//input[@id='v-0-4-0-0-7']")
+    private WebElement accountInfoPageLastNameInputField;
+    @FindBy(xpath = "//main//section[2]//input[@id='v-0-4-0-0-13']")
+    private WebElement accountInfoPageEmailInputField;
+    //password section
+    @FindBy(xpath = "//main//section[3]//input[@id='v-0-4-0-0-25']")
+    private WebElement accountInfoPagePasswordInputField;
+    @FindBy(xpath = "//main//section[3]//input[@id='v-0-4-0-0-31']")
+    private WebElement accountInfoPageNewPasswordInputField;
+    @FindBy(xpath = "//main//section[3]//input[@id='v-0-4-0-0-37']")
+    private WebElement accountInfoPageConfirmNewPasswordInputField;
 
     public AccountInfoPageInvalidSingularInputFormat(WebDriver driver) {super(driver);}
 
