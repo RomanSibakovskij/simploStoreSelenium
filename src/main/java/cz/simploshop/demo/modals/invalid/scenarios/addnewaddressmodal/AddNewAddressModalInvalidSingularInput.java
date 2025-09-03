2a00e52bcffe0758ae347b8e75f3f848e02c21c8
RomanSibakovskij <benutzer373@gmail.com>
2025-09-03 03:34:38 +0300

    Add empty AddNewAddressModalInvalidSingularInput utility sheet

---
+++ src/main/java/cz/simploshop/demo/modals/invalid/scenarios/addnewaddressmodal/AddNewAddressModalInvalidSingularInput.java
@@ -0,0 +1,15 @@
+package cz.simploshop.demo.modals.invalid.scenarios.addnewaddressmodal;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class AddNewAddressModalInvalidSingularInput extends BasePage{
+
+
+    
+
+    public AddNewAddressModalInvalidSingularInput(WebDriver driver) {super(driver);}
+
+
+
+}
