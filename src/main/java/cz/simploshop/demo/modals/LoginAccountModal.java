747b9e7d21a6e04a19039f21f4e056ed91b53e61
RomanSibakovskij <benutzer373@gmail.com>
2025-09-03 04:07:41 +0300

    Add empty LoginAccountModal sheet

---
+++ src/main/java/cz/simploshop/demo/modals/LoginAccountModal.java
@@ -0,0 +1,12 @@
+package cz.simploshop.demo.modals;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class LoginAccountModal extends BasePage{
+
+
+    public LoginAccountModal(WebDriver driver) {super(driver);}
+
+
+}
