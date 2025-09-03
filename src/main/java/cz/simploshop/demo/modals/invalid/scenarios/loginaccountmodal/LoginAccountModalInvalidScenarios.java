504ba6323cb72b08294e35fa0fb15f82c6a3a78c
RomanSibakovskij <benutzer373@gmail.com>
2025-09-03 04:27:35 +0300

    Add empty LoginAccountModalInvalidScenarios utility sheet

---
+++ src/main/java/cz/simploshop/demo/modals/invalid/scenarios/loginaccountmodal/LoginAccountModalInvalidScenarios.java
@@ -0,0 +1,14 @@
+package cz.simploshop.demo.modals.invalid.scenarios.loginaccountmodal;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class LoginAccountModalInvalidScenarios extends BasePage{
+
+
+
+    public LoginAccountModalInvalidScenarios(WebDriver driver) {super(driver);}
+
+    
+
+}
