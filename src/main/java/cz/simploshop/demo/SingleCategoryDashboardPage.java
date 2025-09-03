5f7b1319c5ca6d768c4041fe7c0f7a1de1f12e02
RomanSibakovskij <benutzer373@gmail.com>
2025-09-03 05:25:57 +0300

    Add empty SingleCategoryDashboardPage sheet

---
+++ src/main/java/cz/simploshop/demo/SingleCategoryDashboardPage.java
@@ -0,0 +1,14 @@
+package cz.simploshop.demo;
+
+import cz.simploshop.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+
+public class SingleCategoryDashboardPage extends BasePage {
+
+
+
+    public SingleCategoryDashboardPage(WebDriver driver) {super(driver);}
+
+
+
+}
