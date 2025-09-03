089d38240a6cf411b1645e2cfc697e2d38b26fe0
RomanSibakovskij <benutzer373@gmail.com>
2025-09-03 04:28:49 +0300

    Add relevant web elements for LoginAccountModalInvalidScenarios utility sheet

--- src/main/java/cz/simploshop/demo/modals/invalid/scenarios/loginaccountmodal/LoginAccountModalInvalidScenarios.java
+++ src/main/java/cz/simploshop/demo/modals/invalid/scenarios/loginaccountmodal/LoginAccountModalInvalidScenarios.java
@@ -2,13 +2,18 @@ package cz.simploshop.demo.modals.invalid.scenarios.loginaccountmodal;
 
 import cz.simploshop.demo.utilities.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginAccountModalInvalidScenarios extends BasePage{
 
-
+    //relevant web elements
+    @FindBy(xpath = "//div[@class='sim-auth-form__form']//form//input[@id='v-0-3-38']")
+    private WebElement loginAccountModalEmailInputField;
+    @FindBy(xpath = "//div[@class='sim-auth-form__form']//form//input[@id='v-0-3-44']")
+    private WebElement loginAccountModalPasswordInputField;
 
     public LoginAccountModalInvalidScenarios(WebDriver driver) {super(driver);}
 
-    
+
 
 }
