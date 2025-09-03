105652ea76ae73305f96e82a17b1a1d627a75b35
RomanSibakovskij <benutzer373@gmail.com>
2025-09-03 04:55:27 +0300

    Add empty ShoppingCartModal sheet

---
+++ src/main/java/cz/simploshop/demo/modals/ShoppingCartModal.java
@@ -0,0 +1,13 @@
+package cz.simploshop.demo.modals;
+
+import cz.simploshop.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+
+public class ShoppingCartModal extends BasePage {
+
+
+
+    public ShoppingCartModal(WebDriver driver) {super(driver);}
+    
+
+}
