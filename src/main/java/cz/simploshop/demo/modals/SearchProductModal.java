63b162fae420a2dcf51f42c5e5502f21904a4027
RomanSibakovskij <benutzer373@gmail.com>
2025-09-04 05:01:11 +0300

    Add empty SearchProductModal sheet

---
+++ src/main/java/cz/simploshop/demo/modals/SearchProductModal.java
@@ -0,0 +1,12 @@
+package cz.simploshop.demo.modals;
+
+import cz.simploshop.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+
+public class SearchProductModal extends BasePage {
+
+
+    public SearchProductModal(WebDriver driver) {super(driver);}
+
+    
+}
