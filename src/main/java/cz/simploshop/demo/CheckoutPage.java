2beace65e5f535e0fdc0b4f2b5dfb9ed704bae57
RomanSibakovskij <benutzer373@gmail.com>
2025-09-04 13:30:25 +0300

    Add empty CheckoutPage sheet

---
+++ src/main/java/cz/simploshop/demo/CheckoutPage.java
@@ -0,0 +1,13 @@
+package cz.simploshop.demo;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class CheckoutPage extends BasePage{
+
+
+    
+    public CheckoutPage(WebDriver driver) {super(driver);}
+
+
+}
