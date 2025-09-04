4a7fb4efceffe168c7277b4e8d71d0c285673e84
RomanSibakovskij <benutzer373@gmail.com>
2025-09-04 06:30:15 +0300

    Add empty AddProductReviewModal sheet

---
+++ src/main/java/cz/simploshop/demo/modals/AddProductReviewModal.java
@@ -0,0 +1,13 @@
+package cz.simploshop.demo.modals;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class AddProductReviewModal extends BasePage{
+
+
+
+    public AddProductReviewModal(WebDriver driver) {super(driver);}
+
+
+}
