7d7ec0a3bf4671031e28d126f321a32f1ec3eaf3
RomanSibakovskij <benutzer373@gmail.com>
2025-09-04 07:03:48 +0300

    Add empty AddProductReviewModalInvalidScenarios utility sheet

---
+++ src/main/java/cz/simploshop/demo/modals/invalid/scenarios/AddProductReviewModalInvalidScenarios.java
@@ -0,0 +1,13 @@
+package cz.simploshop.demo.modals.invalid.scenarios;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class AddProductReviewModalInvalidScenarios extends BasePage{
+
+
+
+    public AddProductReviewModalInvalidScenarios(WebDriver driver) {super(driver);}
+
+
+}
