7e5809bce374f747e9ca1d81b9afdcdd0707f6b0
RomanSibakovskij <benutzer373@gmail.com>
2025-09-04 07:49:32 +0300

    Add empty ShoppingCartPage sheet

---
+++ src/main/java/cz/simploshop/demo/ShoppingCartPage.java
@@ -0,0 +1,12 @@
+package cz.simploshop.demo;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class ShoppingCartPage extends BasePage{
+
+
+    public ShoppingCartPage(WebDriver driver) {super(driver);}
+
+
+}
