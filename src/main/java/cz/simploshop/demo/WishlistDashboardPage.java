c1e4864f3ae05665368e5a09b8ab7960008d1f0f
RomanSibakovskij <benutzer373@gmail.com>
2025-09-04 05:45:41 +0300

    Add empty WishlistDashboardPage sheet

---
+++ src/main/java/cz/simploshop/demo/WishlistDashboardPage.java
@@ -0,0 +1,13 @@
+package cz.simploshop.demo;
+
+import cz.simploshop.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+
+public class WishlistDashboardPage extends BasePage {
+
+
+
+    public WishlistDashboardPage(WebDriver driver) {super(driver);}
+
+
+}
