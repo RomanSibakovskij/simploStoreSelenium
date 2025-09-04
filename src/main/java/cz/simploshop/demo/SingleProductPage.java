ddf83060545ea9f3164eeee46d9e574d2179a033
RomanSibakovskij <benutzer373@gmail.com>
2025-09-04 05:08:46 +0300

    Add empty SingleProductPage sheet

---
+++ src/main/java/cz/simploshop/demo/SingleProductPage.java
@@ -0,0 +1,13 @@
+package cz.simploshop.demo;
+
+import cz.simploshop.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+
+public class SingleProductPage extends BasePage {
+
+
+
+    public SingleProductPage(WebDriver driver) {super(driver);}
+
+
+}
