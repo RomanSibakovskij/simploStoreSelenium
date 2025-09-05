c8f2be6fae1197f93ed2bf4364282f07a4f82299
RomanSibakovskij <benutzer373@gmail.com>
2025-09-05 05:55:22 +0300

    Add Invalid Single Product Checkout Confirmation Test (as a guest) - Too Long Billing Email test method

--- src/main/java/cz/simploshop/demo/invalidscenarios/checkoutpage/CheckoutPageTooLongSingularInput.java
+++ src/main/java/cz/simploshop/demo/invalidscenarios/checkoutpage/CheckoutPageTooLongSingularInput.java
@@ -35,7 +35,43 @@ public class CheckoutPageTooLongSingularInput extends BasePage{
     @FindBy(xpath = "//form[2]/div[4]/form//div[@class='sim-form-row'][5]//div[@class='sim-form-group w-full']//input")
     private WebElement checkoutPageShippingPhoneInputField;
 
+    //invalid billing address data - too long singular input
+    private String tooLongCheckoutBillAddressEmail;
+
+    //valid billing address data - for remaining inputs
+    private String validCheckoutBillAddressEmail;
+    private String validCheckoutBillAddressFirstName;
+    private String validCheckoutBillAddressLastName;
+    private String validCheckoutBillAddressStreet;
+    private String validCheckoutBillAddressCity;
+    private int validCheckoutBillAddressZipCode;
+    private String validCheckoutBillAddressPhone;
+
     public CheckoutPageTooLongSingularInput(WebDriver driver) {super(driver);}
 
+    //invalid billing address (guest) data input getter - too long email (75 chars -> name, domain)
+    public void invalidCheckoutBillAddressTooLongEmailInputDataGetter(){
+
+        tooLongCheckoutBillAddressEmail = TestDataGenerator.generateRandomTooLongEmailAddress(75);
+
+        System.out.println("Invalid checkout billing address too long email input data: " + "\n");
+
+        logger.info("Too long checkout billing address email: " + tooLongCheckoutBillAddressEmail);
+
+        System.out.println("\n");
+
+    }
+
+    //invalid billing address (guest) input data methods - too long singular input
+    public void inputTooLongCheckoutEmailIntoEmailInputField(){checkoutPageEmailInputField.sendKeys(tooLongCheckoutBillAddressEmail);}
+
+    //valid billing address (guest) input data methods
+    public void inputValidCheckoutEmailIntoEmailInputField(){checkoutPageEmailInputField.sendKeys(validCheckoutBillAddressEmail);}
+    public void inputValidCheckoutFirstNameIntoFirstNameInputField(){checkoutPageBillingFirstNameInputField.sendKeys(validCheckoutBillAddressFirstName);}
+    public void inputValidCheckoutLastNameIntoLastNameInputField(){checkoutPageBillingLastNameInputField.sendKeys(validCheckoutBillAddressLastName);}
+    public void inputValidCheckoutStreetIntoStreetInputField(){checkoutPageBillingStreetInputField.sendKeys(validCheckoutBillAddressStreet);}
+    public void inputValidCheckoutCityIntoCityInputField(){checkoutPageBillingCityInputField.sendKeys(validCheckoutBillAddressCity);}
+    public void inputValidCheckoutZipCodeIntoZipCodeInputField(){checkoutPageBillingZipCodeInputField.sendKeys(String.valueOf(validCheckoutBillAddressZipCode));}
+    public void inputValidCheckoutPhoneIntoPhoneInputField(){checkoutPageBillingPhoneInputField.sendKeys(validCheckoutBillAddressPhone);}
 
 }
---
+++ src/test/java/cz/simploshop/demo/invalidscenarios/checkoutpage/CheckoutPageTooLongSingularInputTest.java
@@ -0,0 +1,27 @@
+package cz.simploshop.demo.invalidscenarios.checkoutpage;
+
+import cz.simploshop.demo.utilities.TestMethods;
+import org.junit.jupiter.api.*;
+
+public class CheckoutPageTooLongSingularInputTest extends TestMethods{
+
+    //invalid chosen product checkout confirmation tests
+
+    //too long singular input
+
+    //Test 022ad -> invalid single product ("Polštář Furniture") checkout confirmation test - too long billing email (75 chars -> name, domain)
+    @Test
+    @DisplayName("Invalid Single Product Checkout Confirmation Test (as a guest) - Too Long Billing Email")
+    @Tag("Invalid_Single_Product_Checkout_Confirmation")
+    @Tag("Too_Long_Singular_Input")
+    @Tag("Test_As_A_Guest")
+    void invalidSingleProductCheckoutGuestTooLongEmailTest() {
+        //add single chosen product ("Polštář Furniture") to cart test - as a guest
+        addSingleChosenProductToCartTest();
+        //add single chosen product ("Polštář Furniture") to check out test - as a guest
+        addProductToCheckoutTest();
+        //invalid single product ("Polštář Furniture") checkout confirmation test - too long billing email (75 chars -> name, domain)
+        invalidProductCheckoutConfirmationGuestTooLongEmailTest();
+    }
+
+}
