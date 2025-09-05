b69791a6595b39cc0eccff76b562856abacfc47d
RomanSibakovskij <benutzer373@gmail.com>
2025-09-05 05:46:22 +0300

    Add empty CheckoutPageTooLongSingularInput utility sheet

---
+++ src/main/java/cz/simploshop/demo/invalidscenarios/checkoutpage/CheckoutPageTooLongSingularInput.java
@@ -0,0 +1,13 @@
+package cz.simploshop.demo.invalidscenarios.checkoutpage;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class CheckoutPageTooLongSingularInput extends BasePage{
+
+
+
+    public CheckoutPageTooLongSingularInput(WebDriver driver) {super(driver);}
+
+
+}
