86fa9bedf83d2c78884191565de8bdce0ff793fa
RomanSibakovskij <benutzer373@gmail.com>
2025-09-06 04:36:52 +0300

    Add empty CheckoutPageInvalidSingularInput utility sheet

---
+++ src/main/java/cz/simploshop/demo/invalidscenarios/checkoutpage/CheckoutPageInvalidSingularInput.java
@@ -0,0 +1,13 @@
+package cz.simploshop.demo.invalidscenarios.checkoutpage;
+
+import cz.simploshop.demo.utilities.*;
+import org.openqa.selenium.*;
+
+public class CheckoutPageInvalidSingularInput extends BasePage{
+
+
+
+    public CheckoutPageInvalidSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
